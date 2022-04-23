2a2c0528111f14896a294e8a7b4957bba18d78c0
Hygge <3163367790@qq.com>
2022-04-23 13:08:46 +0800

    修改数据库

---
+++ vblog/初始化数据库.sql
@@ -0,0 +1,218 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : MySQL
+ Source Server Type    : MySQL
+ Source Server Version : 50736
+ Source Host           : MySQL:3306
+ Source Schema         : myblog
+
+ Target Server Type    : MySQL
+ Target Server Version : 50736
+ File Encoding         : 65001
+
+ Date: 23/04/2022 12:45:57
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for other_log
+-- ----------------------------
+DROP TABLE IF EXISTS `other_log`;
+CREATE TABLE `other_log`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '日志名称',
+  `type` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '日志类型',
+  `ip` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '主机ip',
+  `user_id` int(11) NULL DEFAULT NULL COMMENT '用户主键',
+  `class_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '类名称',
+  `mothod` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '方法名',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `succeed` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '是否成功',
+  `params` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '参数',
+  `remarks` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 12 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '日志表';
+
+-- ----------------------------
+-- Records of other_log
+-- ----------------------------
+
+
+-- ----------------------------
+-- Table structure for v_article
+-- ----------------------------
+DROP TABLE IF EXISTS `v_article`;
+CREATE TABLE `v_article`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '文章id',
+  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '标题',
+  `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '描述',
+  `context` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '内容',
+  `number_view` int(11) NULL DEFAULT 0 COMMENT '浏览量',
+  `user_id` int(11) NULL DEFAULT NULL COMMENT '用户id',
+  `categorys_id` int(11) NULL DEFAULT 1 COMMENT '分类id 默认分类1',
+  `created_date` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `status` int(2) NOT NULL COMMENT '文章状态 0回收站 1已发布',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 23 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '文章表';
+
+-- ----------------------------
+-- Records of v_article
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for v_categorys
+-- ----------------------------
+DROP TABLE IF EXISTS `v_categorys`;
+CREATE TABLE `v_categorys`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '分类名',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '分类表';
+
+-- ----------------------------
+-- Records of v_categorys
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for v_comment
+-- ----------------------------
+DROP TABLE IF EXISTS `v_comment`;
+CREATE TABLE `v_comment`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '内容',
+  `user_id` int(11) NULL DEFAULT NULL COMMENT '用户id',
+  `article_id` int(11) NOT NULL COMMENT '文章id',
+  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '评论昵称',
+  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '评论者邮箱',
+  `address` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '链接',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '评论表';
+
+-- ----------------------------
+-- Records of v_comment
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for v_file_record
+-- ----------------------------
+DROP TABLE IF EXISTS `v_file_record`;
+CREATE TABLE `v_file_record`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `file_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '文件名',
+  `path` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '文件路径',
+  `file_url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '文件地址或者链接',
+  `type` tinyint(4) NULL DEFAULT NULL COMMENT '文件类型 0图片 ',
+  `file_suffix_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '文件后缀',
+  `local_or_cloud` tinyint(4) NULL DEFAULT NULL COMMENT '本地文件还是云存储 0本地 1云',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '文件存储记录表';
+
+-- ----------------------------
+-- Records of v_file_record
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for v_friend_link
+-- ----------------------------
+DROP TABLE IF EXISTS `v_friend_link`;
+CREATE TABLE `v_friend_link`  (
+  `id` int(11) NOT NULL,
+  `user_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '昵称',
+  `avator` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '头像链接',
+  `address` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '网站链接',
+  `miaosu` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '描述',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '友情链接';
+
+-- ----------------------------
+-- Records of v_friend_link
+-- ----------------------------
+-- ----------------------------
+-- Table structure for v_login
+-- ----------------------------
+DROP TABLE IF EXISTS `v_login`;
+CREATE TABLE `v_login`  (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '登录邮箱',
+  `ip` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '登录ip',
+  `create_date` datetime NULL DEFAULT NULL COMMENT '登录时间',
+  `address` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '登录地址',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '用户登录记录表';
+
+-- ----------------------------
+-- Records of v_login
+-- ----------------------------
+-- ----------------------------
+-- Table structure for v_sys_config
+-- ----------------------------
+DROP TABLE IF EXISTS `v_sys_config`;
+CREATE TABLE `v_sys_config`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `param_key` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'key',
+  `param_value` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '具体参数json形式',
+  `remark` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '具体描述',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '系统配置';
+
+-- ----------------------------
+-- Records of v_sys_config
+-- ----------------------------
+
+
+-- ----------------------------
+-- Table structure for v_tag
+-- ----------------------------
+DROP TABLE IF EXISTS `v_tag`;
+CREATE TABLE `v_tag`  (
+  `id` int(11) NOT NULL,
+  `tag_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '标签名',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '标签表';
+
+-- ----------------------------
+-- Records of v_tag
+-- ----------------------------
+
+
+-- ----------------------------
+-- Table structure for v_tag_relationship
+-- ----------------------------
+DROP TABLE IF EXISTS `v_tag_relationship`;
+CREATE TABLE `v_tag_relationship`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `article_id` int(11) NULL DEFAULT NULL COMMENT '文章id',
+  `tag_id` int(11) NULL DEFAULT NULL COMMENT '标签id',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 21 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '文章与标签联系表';
+
+-- ----------------------------
+-- Records of v_tag_relationship
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for v_user
+-- ----------------------------
+DROP TABLE IF EXISTS `v_user`;
+CREATE TABLE `v_user`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户id',
+  `user_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户名',
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '密码',
+  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '邮箱',
+  `avator` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '头像',
+  `qq` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'QQ',
+  `created_date` datetime NOT NULL COMMENT '创建时间',
+  `icp` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '备案号',
+  `signature` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '签名',
+  `count` int(11) NULL DEFAULT 0 COMMENT '网站浏览量',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '用户表';
+
+-- ----------------------------
+-- Records of v_user
+-- ----------------------------
+
+SET FOREIGN_KEY_CHECKS = 1;
